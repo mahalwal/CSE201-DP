912a20a3709a5d002575f443aaea6b74c0c53447
mahalwal <manish16054@iiitd.ac.in>
2017-10-28 22:47:42 +0530

    Better GUI Added

--- Static GUI/CSE201_v1/src/address/MainApp.java
+++ Static GUI/CSE201_v1/src/address/MainApp.java
@@ -1,42 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package address;
 
 import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Mahalwal's
- */
 public class MainApp extends Application {
-    
-    public Stage primaryStage;
-    public BorderPane rootLayout;
-    
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Login");
-        
+        this.primaryStage.setTitle("AddressApp");
+
         initRootLayout();
-        showLogin();
+
+        showPersonOverview();
     }
-    
-    public void initRootLayout()
-    {
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
@@ -51,12 +43,16 @@ public class MainApp extends Application {
             e.printStackTrace();
         }
     }
-    public void showLogin(){
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    public void showPersonOverview() {
         try {
             // Load person overview.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/LoginOverview.fxml"));
-            AnchorPane personOverview = (AnchorPane) loader.load();
+            loader.setLocation(MainApp.class.getResource("view/loginOverview.fxml"));
+            StackPane personOverview = (StackPane) loader.load();
 
             // Set person overview into the center of root layout.
             rootLayout.setCenter(personOverview);
@@ -66,10 +62,14 @@ public class MainApp extends Application {
     }
 
     /**
-     * @param args the command line arguments
+     * Returns the main stage.
+     * @return
      */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
